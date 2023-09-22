bc333a9a49211f3f1a45093540c96c8b4e4f2973
Leejun <Leejun@DESKTOP-1TV2PBR>
2023-09-22 10:34:32 +0900

    4번 최종수정 완료
    이슈 없음..폴더를 안만들어서 만들었음

--- quiz1_LJY/src/main/java/edu/mit/controller/QuizController.java
+++ quiz1_LJY/src/main/java/edu/mit/controller/QuizController.java
@@ -5,14 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.extern.log4j.Log4j;
 
 @Controller
-@Log4j
 @RequestMapping("/quiz/**")
 public class QuizController {
 	
-	@GetMapping("/quiz1")
+	@GetMapping("quiz1")
 	public void quiz1(Model m) {
 		m.addAttribute("quiz1","오늘은 금요일");
 		
